40755faf9b098955d64faa7fa278dfcc5f61a44b
Eduardo Cavichioli <edcavichioli@gmail.com>
2020-02-21 20:17:22 -0300

    adding expandable row

--- src/components/dataTable/DataTable.js
+++ src/components/dataTable/DataTable.js
@@ -5,6 +5,8 @@ import { geneDataType } from '../../helpers/types';
 function DataTable(props) {
   const { data } = props;
   const [rows, setRows] = useState(<></>);
+  const [expandedRows, setExpandedRows] = useState([]);
+  
   const headers = (
     <tr>
       <th> </th>
@@ -15,19 +17,46 @@ function DataTable(props) {
     </tr>
   );
 
-  useEffect(() => {
-    const sortedData = [ ...data ];
-    sortedData.sort((a, b) => b.association_score.overall - a.association_score.overall);
-    setRows(sortedData.map((item) => (
+  const handleExpandRow = (rowId) => {
+    const isRowExpanded = expandedRows.includes(rowId);
+
+    const newExpandedRows = isRowExpanded
+      ? expandedRows.filter((id) => id !== rowId)
+      : expandedRows.concat(rowId);
+
+    setExpandedRows(newExpandedRows);
+  };
+
+  const renderRow = (item) => {
+    const isRowExpanded = expandedRows.includes(item.id);
+    const expandedLabel = isRowExpanded ? '-' : '+';
+
+    const itemRow = [
       <tr key={item.id} >
-        <td className="expand" >+</td>
+        <td className="expand" onClick={() => handleExpandRow(item.id)}>{expandedLabel}</td>
         <td>{item.target.gene_info.symbol}</td>
         <td>{item.target.id}</td>
         <td>{item.target.gene_info.name}</td>
         <td>{item.association_score.overall}</td>
       </tr>
-    )));
-  }, [data]);
+    ];
+
+    if (isRowExpanded) {
+      itemRow.push(
+        <tr key={`${item.id}-expanded`} >
+          <td colSpan="5" ><div>teste</div></td>
+        </tr>
+      );
+    }
+    
+    return itemRow;
+  };
+
+  useEffect(() => {
+    const sortedData = [...data];
+    sortedData.sort((a, b) => b.association_score.overall - a.association_score.overall);
+    setRows(sortedData.map((item) => renderRow(item)));
+  }, [data, expandedRows]);
 
   return (
     <table>
